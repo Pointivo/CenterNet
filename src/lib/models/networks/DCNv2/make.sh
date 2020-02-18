4be6eb717703a096256870d96f5521de6f23a625
Adnan Chaudhry <adnan@pointivo.com>
2020-02-18 18:27:24 +0500

    Fixed make script and some minor updates

--- src/lib/models/networks/DCNv2/make.sh
+++ src/lib/models/networks/DCNv2/make.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+export CUDA_PATH=/usr/local/cuda-9.0/
+export CXXFLAGS="-std=c++11"
+export CFLAGS="-std=c99"
+
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export CPATH=/usr/local/cuda-9.0/include${CPATH:+:${CPATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
 cd src/cuda
 
 # compile dcn
